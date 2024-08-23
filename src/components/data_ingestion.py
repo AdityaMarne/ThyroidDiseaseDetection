26e3dfd4a1d34462484d4f952406b5c7c9cf4ee1
AdityaMarne <adityamarne2001@gmail.com>
2024-08-23 23:52:04 +0530

    data transformation commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,8 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+
 
 ## Intitialize the Data Ingetion Configuration
 
@@ -22,6 +24,8 @@ class DataIngestion:
         logging.info("Data Ingestion Method Started")
         try:
             
+            #base_dir = r'd:\iNeuronPyTasks\InternShipProject\ThyroidDiseaseDetection'
+
             df = pd.read_csv(os.path.join('notebooks/data','finalTrain_new2.csv'))
             logging.info("Dataset read as pandas DataFrame")
 
@@ -46,3 +50,11 @@ class DataIngestion:
         except Exception as e:
             logging.info('Exception occured at Data Ingestion stage')
             raise CustomException(e,sys)
+        
+
+#Run data ingestion
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data_path,test_data_path = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
--- src/exception.py
+++ src/exception.py
@@ -1,19 +1,18 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_details:sys):
-    _,_,exc_tb = error_details.exc_info()
+def error_message_detail(error,error_detail):
+    _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
 
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
+    error_message = f"Error occurred in Python script '{file_name}' at line {line_number}: {error}"
 
     return error_message
 
 class CustomException(Exception):
     
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
